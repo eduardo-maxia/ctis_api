a65bfa6490f755d7ad73cd5d3e6041a4866ac59b
Eduardo <eduardorodriguesanjos@gmail.com>
2022-11-21 21:09:11 -0300

    a

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -225,7 +225,7 @@ class NotificacaoView(APIView):
         if request.data.get('data_vencimento'):
             _mes_referencia = Mes.objects.get(mes = current_date.month, ano_letivo = current_date.year)
             _pagamentos_pendentes = TurmaAlunoPagamento.objects.select_related('turma_aluno__pessoa_aluno').filter(
-                mes_referencia = _mes_referencia,
+                status__in = [1,2,3], mes_referencia = _mes_referencia,
                 turma_aluno__pessoa_aluno__data_vencimento = request.data.get('data_vencimento')).all()
             
             _notificacoes = [
